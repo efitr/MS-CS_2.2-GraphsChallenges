b433f806b51af1e691a2ed1067500fa57a9f98a3
Egon Fiedler <egonfiedler@Egons-MacBook-Air.local>
2019-07-15 13:57:40 -0700

    Beggining of my classwork

---
+++ Challenge1/challenge_1.py
@@ -0,0 +1,5 @@
+# Challenge 1
+## Goals:
+* Implement the Graph ADT with an adjacency list
+* Implement code to read in a graph from a text file to create an instance of the Graph ADT and use it's methods.
+
