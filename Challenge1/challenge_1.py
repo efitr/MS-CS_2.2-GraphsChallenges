1d16af0ceed5b52597203ba4602e87f4e1e29b1b
Egon Fiedler <egonfiedler@Egons-MacBook-Air.local>
2019-07-16 04:05:20 -0700

    Some updates

--- Challenge1/challenge_1.py
+++ Challenge1/challenge_1.py
@@ -28,22 +28,66 @@
 # (1,4,5)
 # (2,3,5)
 # (2,4,7)
+
 # ----------------------------------------------------------
-###########################################################
-###########################################################
-## 
-## Functions required to accomplish the Goal
-###########################################################
+# AbstractDataType origin
+# ----------------------------------------------------------
+
+# from graph.py import Graph
+
+# ----------------------------------------------------------
+# Main functions required to accomplish the Goal
+# ----------------------------------------------------------
+
+def make_graph(data):
+    pass
+
+# ----------------------------------------------------------
+# Helper functions 
+# ----------------------------------------------------------
+
+def vertexes_of_data(data):
+    return data[1].rstrip().split(',')
+
+def _iter_(some_list):
+    pass
+
+def open_file(file_name):
+    file = open(file_name, 'r') 
+    return file.readlines() 
 
 
-
+# --------------------------------------------------------
+# --------------------------------------------------------
+# START OF SCRIPT
 if __name__ == "__main__":
 
-# --------------------------------------------------------
-#  Local Files required
+#  AbstractDataType needed
 # --------------------------------------------------------
 
-# Open F
+# Getting data ready
+# --------------------------------------------------------
+    data = open_file("graph_data.txt")
+    print("This is the data file: ", data)
+    
+# Making the elements
+# -------------------------------------------------------- 
+    vertexes = vertexes_of_data(data)
+    print("Vertices: ", len(vertexes))
+
+    # for item in range(2, len(data)):
+    #     vertex = data[item].rstrip().replace('(', '').replace(')', '').split(',')
+
+# Vertices: 4
+# Edges: 4
+# Edge List:
+# (1,2,10)
+# (1,4,5)
+# (2,3,5)
+# (2,4,7)
+
+# Setting up the answer
+    # for index in 
 
 # --------------------------------------------------------
 #  Data  
