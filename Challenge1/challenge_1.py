81fc30ac4b2996a430719491ee27c65a65eb96d1
Egon Fiedler <egonfiedler@Egons-MacBook-Air.local>
2019-07-15 23:52:19 -0700

    Problem Setup

--- Challenge1/challenge_1.py
+++ Challenge1/challenge_1.py
@@ -1,5 +1,26 @@
 # Challenge 1
 ## Goals:
-* Implement the Graph ADT with an adjacency list
-* Implement code to read in a graph from a text file to create an instance of the Graph ADT and use it's methods.
+# *Implement the Graph ADT with an adjacency list
+# *Implement code to read in a graph from a text file to create an instance of the Graph ADT and use it's methods.
+
+# Inputs:
+# * The graph_data.txt
+# G
+# 1,2,3,4
+# (1,2,10)
+# (1,4,5)
+# (2,3,5)
+# (2,4,7)
+
+# Process required to achieve both end points:
+# Make the Vertex Class
+ 
+# Outputs:
+# Vertices: 4
+# Edges: 4
+# Edge List:
+# (1,2,10)
+# (1,4,5)
+# (2,3,5)
+# (2,4,7)
 
