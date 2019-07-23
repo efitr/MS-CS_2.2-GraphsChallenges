7b431202a578ae8ae55db65cc2193b5ed11686eb
Egon Fiedler <egonfiedler@Egons-MacBook-Air.local>
2019-07-22 17:00:34 -0700

    Set the problem up

--- Challenge2/challenge_2.py
+++ Challenge2/challenge_2.py
@@ -41,34 +41,10 @@
 # Main functions required to accomplish the Goal
 # ----------------------------------------------------------
 
-def make_graph(data):
-    graph_network = Graph()
-    for vertex in vertexes_of_data(data):
-        graph_network.add_vertex(vertex)
-    
-    for adjacency_list in data[2:]:
-        clean_adjacency_list = adjacency_list.rstrip().replace('(', '').replace(')', '').split(',') 
-    return clean_adjacency_list 
-    pass
-
-
 
 # ----------------------------------------------------------
 # Helper functions 
 # ----------------------------------------------------------
-
-def vertexes_of_data(data):
-    return data[1].rstrip().split(',')
-
-def _iter_lists_in_data(data):
-    # adjacency_lists = []
-    for adjacency_list in data[2:]:
-        clean_adjacency_list = adjacency_list.rstrip().replace('(', '').replace(')', '').split(',') 
-    return clean_adjacency_list
-
-def open_file(file_name):
-    file = open(file_name, 'r') 
-    return file.readlines() 
 
 
 # --------------------------------------------------------
@@ -83,25 +59,3 @@ if __name__ == "__main__":
 # --------------------------------------------------------
     data = open_file("graph_data.txt")
     print("This is the data file: ", data)
-    
-# Making the elements
-# -------------------------------------------------------- 
-    vertexes = vertexes_of_data(data)
-    print("Vertices: ", vertexes, len(vertexes))
-
-
-# Vertices: 4
-# Edges: 4
-# Edge List:
-# (1,2,10)
-# (1,4,5)
-# (2,3,5)
-# (2,4,7)
-
-# Setting up the answer
-    # for index in 
-
-# --------------------------------------------------------
-#  Data  
-# --------------------------------------------------------
-
