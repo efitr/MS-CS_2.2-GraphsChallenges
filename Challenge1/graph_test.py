b3147abad2cb723ad30bc5ca2416dffb90b4d5bf
Egon Fiedler <egonfiedler@Egons-MacBook-Air.local>
2019-08-05 17:31:33 -0700

    Improving the tests

--- Challenge1/graph_test.py
+++ Challenge1/graph_test.py
@@ -44,7 +44,9 @@ class TestVertex(unittest.TestCase):
         self.assertEqual(vertex_c.links, {vertex_a:0})
         self.assertEqual(vertex_a.links, {vertex_b:0})
 
+
     def test_get_links(self):
+        # All vertex
         vertex_a = Vertex('A')
         vertex_b = Vertex('B')
         vertex_c = Vertex('C')  
@@ -52,27 +54,92 @@ class TestVertex(unittest.TestCase):
         vertex_e = Vertex('E')
         vertex_f = Vertex('F')
 
+        while vertex not in self.links:
+            try:
+                self.links[vertex] = weight
+                self.num_links += 1 
+                return
+            except ValueError:
+                print("Vertex", vertex, "is already inside")
+
+        def _checking_catch_ValueError(self,ghost_vertex):
+            ## Vertex or Node, category DataStructure, AdjacentList
+
+            # vertex_a            
+            ### Simulation of adding repeated links 
+            counter = 0
+            while not ValueError:
+
+                counter += 1
+                if counter == 100:
+                    return "Infinite loop"
+            return True
+                    while vertex not in self.links:
+            try:
+                self.links[vertex] = weight
+                self.num_links += 1 
+                return
+            except ValueError:
+                print("Vertex", vertex, "is already inside")
+
+        ghost_vertex = Vertex('ghost_vertex')
+
+        dummy = _checking_catch_ValueError(ghost_vertex)
+
+        assert dummy is True
+
+
+
+        ### Vertex 'A' => Vertex 'F'
+        vertex_a.add_link(vertex_f)
+        
+        ### Raise value error
+        vertex_a.add_link(vertex_f)
         ## Links of A
-        # Vertex 'A' => Vertex 'B'
+        ### Vertex 'A' => Vertex 'B'
         vertex_a.add_link(vertex_b)
+        ### Vertex 'A' => Vertex 'C'
+        vertex_a.add_link(vertex_c)
 
         ## Links of B
-        # Vertex 'A' => Vertex 'B'
+        ### Vertex 'B' => Vertex 'A'
         vertex_b.add_link(vertex_a)
-        # Vertex 'B' => Vertex 'C'
+        ### Vertex 'B' => Vertex 'C'
         vertex_b.add_link(vertex_c) 
+        ### Vertex 'B' => Vertex 'D'
+        vertex_b.add_link(vertex_d)
+        ### Vertex 'B' => Vertex 'E'
+        vertex_b.add_link(vertex_e) 
 
         ## Links of C
-        # Vertex 'C' => Vertex 'D'
+        ### Vertex 'C' => Vertex 'D'
         vertex_c.add_link(vertex_d)
+        ### Vertex 'C' => Vertex 'B'
+        vertex_c.add_link(vertex_b)
+        ### Vertex 'C' => Vertex 'F'
+        vertex_c.add_link(vertex_f)
+        ### Vertex 'C' => Vertex 'E'
+        vertex_c.add_link(vertex_e)
 
         ## Links of D
-        # Vertex 'D' => Vertex 'E'
+        ### Vertex 'D' => Vertex 'E'
         vertex_d.add_link(vertex_e)
 
         ## Links of E
-        # Vertex 'E' => Vertex 'F'
+        ### Vertex 'E' => Vertex 'F'
         vertex_e.add_link(vertex_f)
+        ### Vertex 'E' => Vertex 'A'
+        vertex_e.add_link(vertex_a)
+        ### Vertex 'E' => Vertex 'B'
+        vertex_e.add_link(vertex_b)
+        ### Vertex 'E' => Vertex 'C'
+        vertex_e.add_link(vertex_c)
+        ### Vertex 'E' => Vertex 'D'
+        vertex_e.add_link(vertex_d)
+
+        ## The actual testing of get_links
+        #Check for the value error
+        # assert ValueError
 
 
     def test_get_id(self):
